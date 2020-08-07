c259f21df43671083e30f034199f68ca8853f556
BlackStar <langzq@sqbj.com>
2020-08-07 19:05:15 +0800

    studyserver 加载 html

--- Server/StudyServer.py
+++ Server/StudyServer.py
@@ -3,7 +3,7 @@
 import socket
 from multiprocessing import Process
 import os
-
+import re
 
 class StudyServer():
     def __init__(self):
@@ -28,15 +28,50 @@ class StudyServer():
             client_socket.close()
 
     def handle_client(self,client_socket):
-        receive_data = client_socket.recv()
-        receive_str = receive_data.decode()
+        receive_data = client_socket.recv(1024)
+        receive_str = receive_data.decode('utf-8')
         receive_lines = receive_str.splitlines()
-        print('*' * 15,'请求内容','*'*15)
-
-        for line in receive_lines:
-            print (line)
-        print('*' * 15, '请求内容', '*'*15)
-
-        print ('\nheader =',receive_lines[0])
-
+       
+        print('*' * 15, 'header', '*'*15)
+        header_line = receive_lines[0]
+        print ('\nheader =',header_line)
+        print('*' * 15, 'header', '*'*15)
     
+        match_result = re.match(r'\w+ +/([^ ]*)',header_line)
+        file_name = match_result.group(1)
+        cwd = os.getcwd()
+        file_path = os.path.join(cwd, 'html',file_name)
+        print('cwd = ',cwd,'file path =',file_path)
+       
+        if file_path.endswith('.html') or file_path.endswith('.css'):
+
+            try:
+                send_file_data = open(file_path, 'rb')
+
+                response_start_line = 'HTTP/1.1 200 OK \n'
+                response_header = 'Server: Study Server \n'
+                response_body = send_file_data.read().decode('utf-8')
+
+            except IOError as identifier:
+                print ('读取文件错误：',identifier)
+                response_start_line = 'HTTP/1.1 404 Not Found \n'
+                response_header = 'Server: Study Server \n'
+                response_body = 'file not found'
+            finally:
+                send_file_data.close()
+                            
+            response_data='%s%s\r\n%s' % (response_start_line,response_header,response_body)
+            print(response_data)
+
+            client_socket.send(response_data.encode('utf-8'))
+            client_socket.close()
+
+
+def main():
+    study_server = StudyServer()
+    study_server.bind(8000)
+    study_server.startServer()
+
+
+if __name__ == '__main__':
+    main()
